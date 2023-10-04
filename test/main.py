bfb9830e75e7c7bd2d28e863622b8d18d778c6b0
lol <felipe.buscaglia@usm.cl>
2023-10-04 12:54:43 -0300

    funciona crear usuario con list constructions

--- test/main.py
+++
@@ -1,6 +0,0 @@
-id = '10'
-constructions = []
-for i in range(10):
-    constructions.append({'posX': i, 'posY': i, 'hasPlant': False, 'plantId': '', 'readyToPlant': False, 'daysTillDone': 2, 'isWatered': False})
-userDict = {'userMicroServiceId': id, 'currentSize': 10, 'maxSize': 20, 'constructions': constructions }
-print(userDict['constructions'][2])
