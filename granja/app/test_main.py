a9ee7229cefa81ec4366bd77f0cf7b7985b09ce7
Felipe Buscaglia <felipe.buscaglia@usm.cl>
2023-11-12 20:48:22 -0300

    test plants con id

--- granja/app/test_main.py
+++ granja/app/test_main.py
@@ -8,4 +8,5 @@ client = TestClient(app)
 def test_read_plants():
     response = client.get("plants")
     assert response.status_code == 200
-    assert response.json()
+    plants = response.json()
+    assert plants[0]["id"] == "65401e4bfe1ebc983d3df03a"
